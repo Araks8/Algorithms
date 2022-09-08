8ab4a4601b53da531aa19dd764552a094b4dd478
Araks8 <arakstigranyan8@gmail.com>
2022-09-08 17:22:43 +0400

    Resolved conflicts

--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 #include "algorithms.h"
 
 int main(){
-  //std::cout << GCD(36,60);
-  std::vector<int> vec {3,5,8,1};
+  std::cout << GCD(36,60);
+  std::vector<int> vec2 {3,5,8,1};
   selection_sort(vec);
-  for(auto i : vec)
-  {
-	std::cout << i;
-  }
 }
