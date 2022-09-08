3f172a23f7515a18c89508238382b94df3995c84
Araks8 <arakstigranyan8@gmail.com>
2022-09-08 17:42:43 +0400

    Deleted unnecessary comment

--- algorithms.h
+++ algorithms.h
@@ -5,7 +5,6 @@
 int GCD(int, int);
 void bubble_sort(std::vector<int>&);
 void selection_sort(std::vector<int>&);
-//void selection_sort(std::vector<int>&);
 //void insertion_sort(std::vector<int>&);
 //void counting_sort(std::vector<int>&);
 //void binary_search(std::vector<int>&);
