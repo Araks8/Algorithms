452d2a3cd22cfb0815954af93bf5f5af52fdf570
Araks8 <arakstigranyan8@gmail.com>
2022-09-08 16:22:19 +0400

    Added Bubble Sort

--- algorithms.h
+++ algorithms.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 int GCD(int, int);
-//void bubble_sort(std::vector<int>&);
+void bubble_sort(std::vector<int>&);
 //void selection_sort(std::vector<int>&);
 //void insertion_sort(std::vector<int>&);
 //void counting_sort(std::vector<int>&);
